e6ffcf7418ab36ac907ed6298e8f047ef1869c03
Bponthemove <bpvanzalk73@gmail.com>
2022-05-25 11:00:52 +0100

    first render

--- src/components/topSection/TopSection.tsx
+++ src/components/topSection/TopSection.tsx
@@ -2,7 +2,6 @@ import React, {useEffect, useState, useRef} from 'react';
 import Image from 'next/image';
 import styles from './TopSection.module.css';
 import backgroundDesktop from '../../../public/images/powder.webp'
-import backgroundPlaceholder from '../../../public/images/powderPlaceholder.webp'
 import menuIcon from '../../../public/icons/k2-logo-white.png'
 import inset from '../../../public/images/powderCap.webp'
 import useGlobalContext from '../../hooks/useGlobalContext';
@@ -19,29 +18,16 @@ export const TopSection = () => {
 
   return (
     <section  className={ styles.topSection }>
-    { !firstRenderRef.current &&
-      <div className={styles.imgContainer} style={{backgroundColor: 'white'}}>
-        <Image  src={backgroundPlaceholder} 
-                alt='snow and ski' 
-                layout='fill' 
-                objectFit='cover' 
-                objectPosition='centre'
-                priority
-        />
-      </div>
-    }
-    { firstRenderRef.current &&
-    <>
       <div  className={h1 ? [styles.h1Container, styles.h1ContainerAnimate].join(' ') : styles.h1Container}
-            style={scroll !== undefined ? {transform: `translateY(${desktop ? scroll/5 : scroll/1.8}%)`} : {}}
+            style={firstRenderRef.current && scroll !== undefined ? {transform: `translateY(${desktop ? scroll/5 : scroll/1.8}%)`} : {}}
       >
         <h1 className={menu ? [styles.h1, styles.h1Grey, styles.h1Active].join(' ') : [styles.h1, styles.h1Grey].join(' ')}
-            style={scroll !== undefined ? {opacity: 1-(scroll/300)} : {opacity: 1}}
+            style={firstRenderRef.current && scroll !== undefined ? {opacity: 1-(scroll/300)} : {opacity: 1}}
         >
           BACKCOUNTRY
         </h1>
         <h1 className={menu ? [styles.h1, styles.h1White, styles.h1Active].join(' ') : [styles.h1, styles.h1White].join(' ')}
-            style={scroll !== undefined ? {opacity: scroll/300} : {opacity: 0}}
+            style={firstRenderRef.current && scroll !== undefined ? {opacity: scroll/300} : {opacity: 0}}
         >
           <span>
             <Image src={ menuIcon } alt='k2 logo' width='75%' height='75%'/>          
@@ -50,7 +36,7 @@ export const TopSection = () => {
         </h1>
       </div>      
       <div  className={menu ? [styles.imgContainer, styles.imgContainerActive].join(' ') : styles.imgContainer}
-            style={scroll !== undefined ? {opacity: `${1 -(scroll/500)}`} : {}}
+            style={firstRenderRef.current && scroll !== undefined ? {opacity: `${1 -(scroll/500)}`} : {}}
       >
         <Image  src={backgroundDesktop} 
                 alt='snow and ski' 
@@ -70,11 +56,10 @@ export const TopSection = () => {
       </div>
       <div 
         className={styles.subTitleMovingUp} 
-        style={scroll !== undefined ? {transform: `translateY(-${scroll/22}%)`} : {}}
+        style={firstRenderRef.current && scroll !== undefined ? {transform: `translateY(-${scroll/22}%)`} : {}}
       >
         Come on... You know you want to! Dreaming of powder days under a bluebird sky? Get ready for your next adventure!
       </div>
-    </>}
     </section>
   );
 };
